09caf462a392fff9f8b2707dc77779287b426772
Saul Reyna <saul.reyna90@gmail.com>
2020-01-27 02:08:21 -0500

    assignment 3 upload

--- Assignment3/src/rna.java
+++ Assignment3/src/rna.java
@@ -1,11 +1,41 @@
-interface IRNA {
+interface ILoString {
   
 }
 
-class MtLoString implements IRNA {
+interface ILoLoString {
+  
+}
+
+class MtLoString implements ILoString {
   MtLoString() { };
 }
 
-class ConsLoString implements IRNA {
+class RNA implements ILoString {
+  String base;
+  ILoString rest;
   
+  RNA(String base, ILoString rest) {
+    this.base = base;
+    this.rest = rest;
+  }
+}
+
+class Protein implements ILoString {
+  String codon;
+  ILoString rest;
+  
+  Protein(String codon, ILoString rest) {
+    this.codon = codon;
+    this.rest = rest;
+  }
+}
+
+class ILoProtein implements ILoLoString {
+  ILoString first;
+  ILoString rest;
+  
+  ILoProtein(ILoString first, ILoString rest) {
+    this.first = first;
+    this.rest = rest;
+  }
 }
