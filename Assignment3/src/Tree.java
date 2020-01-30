85165af26ee20fce49298726d1b386ac09ad92dd
Saul Reyna <saul.reyna90@gmail.com>
2020-01-29 23:46:28 -0500

    finalizing getWidth()

--- Assignment3/src/Tree.java
+++ Assignment3/src/Tree.java
@@ -11,7 +11,16 @@ interface ITree {
 
   boolean isDrooping();
 
-  ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree);
+  ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta,
+      ITree otherTree);
+
+  ITree changeDegree(double theta);
+
+  double getWidth();
+
+  double getWidthHelper(double currWidth);
+
+  double getMaxLength();
 }
 
 class Leaf implements ITree {
@@ -39,7 +48,34 @@ class Leaf implements ITree {
   public ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta,
       ITree otherTree) {
     // TODO Auto-generated method stub
+    return new Branch(leftLength, rightLength, leftTheta, rightTheta,
+        this.changeDegree(180 - (leftTheta - rightTheta)),
+        otherTree.changeDegree(-(90 - rightTheta)));
+  }
+
+  @Override
+  public ITree changeDegree(double theta) {
+    // TODO Auto-generated method stub
     return this;
+  }
+
+  @Override
+  public double getWidth() {
+    // TODO Auto-generated method stub
+    return 2 * this.size;
+  }
+
+  @Override
+  public double getWidthHelper(double currWidth) {
+    // TODO Auto-generated method stub
+    currWidth = currWidth + this.size;
+
+    return currWidth;
+  }
+
+  public double getMaxLength() {
+    // TODO Auto-generated method stub
+    return this.size;
   }
 }
 
@@ -57,30 +93,49 @@ class Stem implements ITree {
     this.tree = tree;
   }
 
-  @Override
   public WorldImage draw() {
     // TODO Auto-generated method stub
     return new OverlayImage(this.tree.draw(),
         new RotateImage(
-            new LineImage(new Posn(this.length, 0), Color.BLACK).movePinhole((this.length / 2), 0), -theta));
+            new LineImage(new Posn(this.length, 0), Color.BLACK).movePinhole((this.length / 2), 0),
+            -theta));
   }
 
-  @Override
   public boolean isDrooping() {
     // TODO Auto-generated method stub
-    if (this.theta > 180 || this.tree.isDrooping()) {
-      return true;
-    }
-    else {
-      return false;
-    }
+    return this.theta > 180 || this.tree.isDrooping();
   }
 
-  @Override
   public ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta,
       ITree otherTree) {
     // TODO Auto-generated method stub
-    return this;
+    return new Branch(leftLength, rightLength, leftTheta, rightTheta,
+        this.changeDegree(180 - (leftTheta - rightTheta)),
+        otherTree.changeDegree(-(90 - rightTheta)));
+  }
+
+  public ITree changeDegree(double theta) {
+    // TODO Auto-generated method stub
+    return new Stem(this.length, this.theta + theta, this.tree);
+  }
+
+  public double getWidth() {
+    // TODO Auto-generated method stub
+    return this.tree.getWidth() 
+        + (Math.abs(Math.cos(Math.toRadians(this.theta)) * this.length));
+  }
+
+  public double getWidthHelper(double currWidth) {
+    // TODO Auto-generated method stub
+    currWidth = currWidth + Math.abs(Math.cos(Math.toRadians(this.theta)) * this.length);
+
+    return this.tree.getWidthHelper(currWidth);
+  }
+
+  @Override
+  public double getMaxLength() {
+    // TODO Auto-generated method stub
+    return this.length;
   }
 
 }
@@ -107,50 +162,100 @@ class Branch implements ITree {
   }
 
   public WorldImage relativePinhole(WorldImage line, double theta, int length) {
-      return new RotateImage(line.movePinhole(length / 2, 0), -theta);
+    return new RotateImage(line.movePinhole(length / 2, 0), -theta);
   }
-  
+
   @Override
   public WorldImage draw() {
     // TODO Auto-generated method stub
     return new OverlayImage(
-        new OverlayImage(this.left.draw(), 
+        new OverlayImage(this.left.draw(),
             relativePinhole(new LineImage(new Posn(this.leftLength, 0), Color.black),
-            this.leftTheta, this.leftLength)).movePinhole(
-                    Math.sin(Math.toRadians(180 - leftTheta)) * this.leftLength,
-                    (Math.cos(Math.toRadians(180 -leftTheta)) * this.leftLength)),
+                this.leftTheta, this.leftLength)).movePinhole(
+                    Math.cos(Math.toRadians(180 - leftTheta)) * this.leftLength,
+                    (Math.sin(Math.toRadians(180 - leftTheta)) * this.leftLength)),
         new OverlayImage(this.right.draw(),
             relativePinhole(new LineImage(new Posn(this.rightLength, 0), Color.black),
                 this.rightTheta, this.rightLength)).movePinhole(
                     -(Math.cos(Math.toRadians(rightTheta)) * this.rightLength),
                     (Math.sin(Math.toRadians(rightTheta)) * this.rightLength)));
-  }  
+  }
 
   @Override
   public boolean isDrooping() {
     // TODO Auto-generated method stub
-    if (this.leftTheta > 180 || this.rightTheta > 180 || 
-        this.left.isDrooping() || this.right.isDrooping()) {
-      return true;
+    return (this.leftTheta > 180) || (this.rightTheta > 180) || this.left.isDrooping()
+        || this.right.isDrooping();
+  }
+
+  public ITree changeDegree(double theta) {
+    return new Branch(this.leftLength, this.rightLength, this.leftTheta + theta,
+        this.rightTheta + theta, this.left.changeDegree(theta), this.right.changeDegree(theta));
+  }
+
+  public ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta,
+      ITree otherTree) {
+    return new Branch(leftLength, rightLength, leftTheta, rightTheta,
+        this.changeDegree(180 - (leftTheta - rightTheta)),
+        otherTree.changeDegree(-(90 - rightTheta)));
+  }
+
+  @Override
+  public double getWidth() {    
+    // TODO Auto-generated method stub
+    double left_width = (Math
+        .abs(this.leftLength * Math.cos(Math.toRadians(180 - this.leftTheta))));
+    double right_width = (Math.abs(this.rightLength * Math.cos(Math.toRadians(this.rightTheta))));
+    return this.left.getWidthHelper(left_width) + this.right.getWidthHelper(right_width);
+  }
+
+  @Override
+  public double getWidthHelper(double currWidth) {
+    // TODO Auto-generated method stub
+    
+      currWidth = currWidth 
+          + (Math.abs(this.getMaxLength() * Math.cos(Math.toRadians(this.getMaxAngle()))));
+      
+      return this.getMaxTree().getWidthHelper(currWidth);
+  }
+
+  @Override
+  public double getMaxLength() {
+    // TODO Auto-generated method stub
+    if (this.leftLength > this.rightLength) {
+      return this.leftLength;
     }
     else {
-      return false;
+      return this.rightLength;
     }
   }
 
-  public ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
-    return new Branch(leftLength, rightLength, leftTheta, rightTheta, this, otherTree);
+  public double getMaxAngle() {
+    if (this.leftLength > this.rightLength) {
+      return 180 - this.leftTheta;
+    }
+    else {
+      return this.rightTheta;
+    }
   }
 
+  public ITree getMaxTree() {
+    if (this.leftLength > this.rightLength) {
+      return this.left;
+    }
+    else {
+      return this.right;
+    }
+  }
 }
 
 class ExamplesTree {
-  ITree tree1 = new Branch(30, 30, 135, 40, new Leaf(5, Color.RED), new Leaf(5, Color.BLUE));
-  ITree tree2 = new Branch(30, 30, 135, 65, new Leaf(5, Color.GREEN), new Leaf(5, Color.ORANGE));
+  ITree tree1 = new Branch(30, 30, 135, 40, new Leaf(10, Color.RED), new Leaf(15, Color.BLUE));
+  ITree tree2 = new Branch(30, 30, 115, 65, new Leaf(15, Color.GREEN), new Leaf(8, Color.ORANGE));
   ITree tree3 = new Stem(40, 90, tree1);
   ITree tree4 = new Stem(50, 90, tree2);
-  ITree tree5 = new Branch(40, 50, 150, 30, tree1, tree2);
-  
+  ITree tree5 = tree1.combine(40, 50, 150, 30, tree2);
+
   WorldImage test = new RotateImage(new LineImage(new Posn(60, 0), Color.black), -150);
 
   boolean testDrawTree(Tester t) {
@@ -158,11 +263,20 @@ class ExamplesTree {
     WorldScene s = new WorldScene(500, 500);
     return c.drawScene(s.placeImageXY(tree5.draw(), 250, 250)) && c.show();
   }
-  
+
   boolean testDrooping(Tester t) {
-    return t.checkExpect(this.tree1.isDrooping(), false) 
+    return t.checkExpect(this.tree1.isDrooping(), false)
         && t.checkExpect(this.tree2.isDrooping(), false)
         && t.checkExpect(this.tree3.isDrooping(), false)
-        && t.checkExpect(this.tree4.isDrooping(), false);
+        && t.checkExpect(this.tree4.isDrooping(), false)
+        && t.checkExpect(this.tree5.isDrooping(), true);
+  }
+
+  boolean testWidth(Tester t) {
+    return t.checkInexact(this.tree1.getWidth(), 69.19, 0.005)
+        && t.checkInexact(this.tree2.getWidth(), 48.35, 0.005)
+        && t.checkInexact(this.tree3.getWidth(), 69.19, 0.005)
+        && t.checkInexact(this.tree4.getWidth(), 48.35, 0.005)
+        && t.checkInexact(this.tree5.getWidth(), 154.8, 0.005);
   }
 }
--- Assignment3/src/rna.java
+++ Assignment3/src/rna.java
@@ -2,31 +2,31 @@ import tester.Tester;
 
 interface ILoString {
   String getCodon();
-  
+
   String getBase();
-  
+
   ILoString getRest();
 
   int countBase();
-  
+
   ILoString convertCodons();
-  
+
   ILoLoString translate();
-  
+
   ILoString findRestCodons();
-  
+
 }
 
 interface ILoLoString {
-  
+
 }
 
 class MtLoString implements ILoString {
-  MtLoString() { };
- 
-  
-  public String getCodon() { 
-    return ""; 
+  MtLoString() {
+  };
+
+  public String getCodon() {
+    return "";
   }
 
   public int countBase() {
@@ -39,13 +39,11 @@ class MtLoString implements ILoString {
     return this;
   }
 
-
   @Override
   public ILoString convertCodons() {
     // TODO Auto-generated method stub
     return this;
   }
-
 
   @Override
   public ILoLoString translate() {
@@ -53,13 +51,11 @@ class MtLoString implements ILoString {
     return null;
   }
 
-
   @Override
   public String getBase() {
     // TODO Auto-generated method stub
     return null;
   }
-
 
   @Override
   public ILoString findRestCodons() {
@@ -72,21 +68,20 @@ class MtLoString implements ILoString {
 class ConsLoString implements ILoString {
   String first;
   ILoString rest;
-  
+
   ConsLoString(String first, ILoString rest) {
     this.first = first;
     this.rest = rest;
   }
-  
+
   public String getBase() {
     return this.first;
   }
-  
+
   public ILoString getRest() {
     return this.rest;
   }
- 
-  
+
   // returns a codon
   public String getCodon() {
     if (this.countBase() >= 3) {
@@ -94,13 +89,13 @@ class ConsLoString implements ILoString {
     }
     else if (this.countBase() == 2) {
       return this.first + this.rest.getBase();
-    } 
+    }
     else if (this.countBase() == 1) {
       return this.first;
-    } 
+    }
     return "";
   }
-  
+
   // returns a list of codons
   public ILoString convertCodons() {
     if (this.countBase() >= 3) {
@@ -108,10 +103,10 @@ class ConsLoString implements ILoString {
     }
     else if (this.countBase() == 2) {
       return new ConsLoString(this.getCodon(), this.getRest().getRest().convertCodons());
-    } 
+    }
     else if (this.countBase() == 1) {
       return new ConsLoString(this.getCodon(), this.getRest().convertCodons());
-    } 
+    }
     return new MtLoString();
   }
 
@@ -120,34 +115,30 @@ class ConsLoString implements ILoString {
     // TODO Auto-generated method stub
     return 1 + this.rest.countBase();
   }
-  
+
   public ILoString findRestCodons() {
-    if (this.first.equals("UAG")
-        || this.first.equals("UAA")
-        || this.first.equals("UGA")
-        || (this.first.length() < 3 )) {
+    if (this.first.equals("UAG") || this.first.equals("UAA") || this.first.equals("UGA")
+        || (this.first.length() < 3)) {
       return new MtLoString();
     }
     else {
       return new ConsLoString(this.first, this.rest.findRestCodons());
     }
   }
-  
-  
-  
+
   public ILoLoString translate() {
     return new ConsLoLoString(this.findRestCodons(), this.rest.translate());
   }
 }
 
 class MtLoProtein implements ILoLoString {
-  
+
 }
 
 class ConsLoLoString implements ILoLoString {
   ILoString first;
   ILoLoString rest;
-  
+
   ConsLoLoString(ILoString first, ILoLoString rest) {
     this.first = first;
     this.rest = rest;
@@ -156,20 +147,20 @@ class ConsLoLoString implements ILoLoString {
 
 class ExamplesRNA {
   ILoString empty = new MtLoString();
-  ILoString RNA1 = new ConsLoString("U", (new ConsLoString("C", new ConsLoString("A", new ConsLoString("G", 
-      new ConsLoString("A",  new ConsLoString("U", new ConsLoString("A", new ConsLoString("G", empty)))))))));
+  ILoString RNA1 = new ConsLoString("U",
+      (new ConsLoString("C", new ConsLoString("A", new ConsLoString("G", new ConsLoString("A",
+          new ConsLoString("U", new ConsLoString("A", new ConsLoString("G", empty)))))))));
   ILoString RNA2 = new ConsLoString("A", new ConsLoString("U", empty));
   ILoString RNA3 = new ConsLoString("A", empty);
-  
+
   boolean testCodon(Tester t) {
-    return t.checkExpect(RNA1.getCodon(), "UCA")
-        && t.checkExpect(RNA2.getCodon(), "AU")
+    return t.checkExpect(RNA1.getCodon(), "UCA") && t.checkExpect(RNA2.getCodon(), "AU")
         && t.checkExpect(RNA3.getCodon(), "A");
   }
-  
+
   boolean testGetListofCodon(Tester t) {
-    return t.checkExpect(RNA1.convertCodons(), 
-        new ConsLoString("UCA", new ConsLoString("GAU", new ConsLoString ("AG", empty))))
+    return t.checkExpect(RNA1.convertCodons(),
+        new ConsLoString("UCA", new ConsLoString("GAU", new ConsLoString("AG", empty))))
         && t.checkExpect(RNA2.convertCodons(), new ConsLoString("AU", empty))
         && t.checkExpect(RNA3.convertCodons(), new ConsLoString("A", empty));
   }
