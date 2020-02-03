e6500f329d43ad090ab25eaf4f8784a058cefc60
Saul Reyna <saul.reyna90@gmail.com>
2020-02-03 12:51:51 -0500

    fixing exceptions

--- Assignment4/src/CakeRecipe.java
+++ Assignment4/src/CakeRecipe.java
@@ -1,88 +1,79 @@
 import tester.Tester;
 
 class CakeRecipe {
-  double flourWeight;
-  double sugarWeight;
-  double eggWeight;
-  double butterWeight;
-  double milkWeight;
+  double flour;
+  double sugar;
+  double egg;
+  double butter;
+  double milk;
   boolean areVolumes = false;
-  
-  CakeRecipe() {
-    this.flourWeight = 0;
-    this.sugarWeight = 0;
-    this.eggWeight = 0;
-    this.butterWeight = 0;
-    this.milkWeight = 0;
-  }
-  
-  CakeRecipe(double flourWeight, double sugarWeight, double eggWeight, double butterWeight, 
-      double milkWeight, boolean areVolumes) {
-    final double EPSILON = 0.1;
+
+  CakeRecipe() { }
+
+  CakeRecipe(double flour, double sugar, double egg, double butter, double milk,
+      boolean areVolumes) {
+    final double epsilon = 0.1;
     if (areVolumes) {
-      flourWeight = flourWeight * 4.25;
-      sugarWeight = sugarWeight * 7;
-      eggWeight = eggWeight * 1.75;
-      butterWeight = butterWeight * 8;
-      milkWeight = milkWeight * 8;
+      flour = flour * 4.25;
+      sugar = sugar * 7;
+      egg = egg * 1.75;
+      butter = butter * 8;
+      milk = milk * 8;
     }
-      
-    if ((Math.abs(flourWeight - sugarWeight) < EPSILON)) {
-      this.flourWeight = flourWeight;
-      this.sugarWeight = sugarWeight;
+
+    if ((Math.abs(flour - sugar) < epsilon)) {
+      this.flour = flour;
+      this.sugar = sugar;
     }
     else {
       throw new IllegalArgumentException("Flour and sugar weights are not equal.");
     }
-    if ((Math.abs(eggWeight - butterWeight) < EPSILON)) {
-      this.eggWeight = eggWeight;
-      this.butterWeight = butterWeight;
+    if ((Math.abs(egg - butter) < epsilon)) {
+      this.egg = egg;
+      this.butter = butter;
     }
     else {
-      System.out.print("egg weight: " + sugarWeight +
-          "\nbutter weight: " + butterWeight + "\n");
       throw new IllegalArgumentException("Egg and butter weights are not equal.");
     }
-    if ((Math.abs((eggWeight + milkWeight) 
-        - sugarWeight) < EPSILON)) {
-      this.milkWeight = milkWeight;
-    } else {
-      throw new IllegalArgumentException("The flour weight is not equal to the total"
-          + " of the milk and egg weights.");
+    if ((Math.abs((egg + milk) - sugar) < epsilon)) {
+      this.milk = milk;
+    }
+    else {
+      throw new IllegalArgumentException(
+          "The flour weight is not equal to the total" + " of the milk and egg weights.");
     }
     this.areVolumes = areVolumes;
   }
 
-  
-  CakeRecipe(double flourWeight, double sugarWeight, double eggWeight, double butterWeight, 
+  CakeRecipe(double flourWeight, double sugarWeight, double eggWeight, double butterWeight,
       double milkWeight) {
     this(flourWeight, sugarWeight, eggWeight, butterWeight, milkWeight, false);
   }
-  
-  CakeRecipe (double flourWeight, double eggWeight, double milkWeight) {
+
+  CakeRecipe(double flourWeight, double eggWeight, double milkWeight) {
     this(flourWeight, (eggWeight + milkWeight), eggWeight, eggWeight, milkWeight, false);
   }
-  
+
   CakeRecipe(double flourWeight, double eggWeight, double milkWeight, boolean areVolumes) {
     this(flourWeight, (eggWeight + milkWeight), eggWeight, eggWeight, milkWeight, areVolumes);
   }
-  
+
   boolean sameRecipe(CakeRecipe other) {
-    final double EPSILON = 0.001;
-    return ((Math.abs(this.flourWeight - other.flourWeight) < EPSILON)
-        && (Math.abs(this.sugarWeight - other.sugarWeight) < EPSILON)
-        && (Math.abs(this.eggWeight - other.eggWeight) < EPSILON)
-        && (Math.abs(this.butterWeight - other.butterWeight) < EPSILON)
-        && (Math.abs(this.milkWeight - other.milkWeight) < EPSILON));
+    final double epsilon = 0.001;
+    return ((Math.abs(this.flour - other.flour) < epsilon)
+        && (Math.abs(this.sugar - other.sugar) < epsilon)
+        && (Math.abs(this.egg - other.egg) < epsilon)
+        && (Math.abs(this.butter - other.butter) < epsilon)
+        && (Math.abs(this.milk - other.milk) < epsilon));
   }
 }
-  
+
 class ExamplesCakes {
   CakeRecipe cake1 = new CakeRecipe(7, 7, 6, 6, 1);
   CakeRecipe cake2 = new CakeRecipe(7, 7, 6, 6, 1);
   CakeRecipe cake3 = new CakeRecipe(1.647, 1, 3.4285, 0.75, 0.125, true);
   CakeRecipe cake4 = new CakeRecipe(4, 2, 2);
-  
+
   boolean testSame(Tester t) {
     return t.checkExpect(this.cake1.sameRecipe(cake2), true)
         && t.checkExpect(this.cake1.sameRecipe(cake3), true)
